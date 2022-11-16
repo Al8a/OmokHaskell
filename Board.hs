237618d5c524e3938ea5d0729fe4f57e39a80492
Al8a <35094272+Al8a@users.noreply.github.com>
2022-11-16 14:52:41 -0700

    Upload current version of files

---
+++ Board.hs
@@ -0,0 +1,200 @@
+module Board where 
+    -- Part 1: Creating a board and accessing its elements
+    
+    {-
+    Return an empty nxn board, where n is a positive number. 
+    A 1-based pair of indices (x,y) will be used to access a specific place of the board
+    x and y are column and row indices -}
+
+    mkBoard :: Int -> [[Int]]                                           
+    mkBoard n = [[0 | x <- [1 .. n]] | y <- [1 .. n]]  
+
+
+    {-
+    Create and return the first player. 
+    Player = 1. 
+    Refer to the first player -}
+
+    mkPlayer :: Int
+    mkPlayer = 1
+
+
+    {-
+    Create and return the second player/rival 
+    Opponent = 2 -}
+
+    mkOpponent :: Int
+    mkOpponent = 2
+  
+
+    -- Return the size of a board bd, n for an nxn board.
+    size :: [[Int]] -> Int
+    size bd = length bd 
+
+
+    {- 
+    Return a row y of a board bd, where y is a 1-based index. 
+    It returns a list of size n, where n is the size of bd. -}
+
+    row :: Int -> [[Int]] -> [Int]
+    row _ [[]] = []                                                             
+    row y (h : t)                                                               
+        | y == 1 = h                                                            
+        | otherwise = row (y - 1) t     
+
+
+    {-
+    Return a column x of a board bd, where x is a 1-based index.
+    It returns a list of size n, where n is the size of bd. -}
+                                                                                                   
+    col :: Int -> [[Int]] -> [Int]                                              
+    col x bd = [row n bd !! (x - 1) | n <- [1 .. size bd]]                    
+
+
+    -- Return diagonal at indicated (x, y) place.                               
+    diagonal :: Int -> Int -> [[Int]] -> [Int]                                  
+    diagonal x y bd                                                             
+        | (y - 1) < size bd && (x - 1) < size bd = row y bd !! (x - 1) : diagonal (x + 1) (y + 1) bd                                                         
+        | otherwise = []                   
+    
+    ------------------------------------------------------------------------------------
+    -- Part 2: Checking places and placing stones     
+
+    {-
+    Mark a place (x,y) in a board bd by a player p 
+    Where x and y are 1-based column and row indices.
+    The specified place is assumed to be empty. -}
+
+    mark :: Int -> Int -> [[Int]] -> Int -> [[Int]]
+    mark x y (h:t) p
+        | y == 1 = markRow x h p : t
+        | otherwise = h : mark x (y-1) t p
+    
+    markRow :: Int -> [Int] -> Int -> [Int]                                     
+    markRow n (h : t) p                                                         
+        | n == 1 = p : t                                                        
+        | otherwise = h : markRow (n - 1) t p  
+         
+                                                                {-
+    Is a place (x,y) of a board bd unmarked or a stone not placed? 
+    The x and y are 1-based column and row indices. -}   
+
+    isEmpty :: Int -> Int -> [[Int]] -> Bool
+    isEmpty x y bd = place == 0
+        where place = row y bd !! (x-1)
+
+    {-
+    Does a place (x,y) of a board bd have a stone placed by a player p?
+    The x and y are 1-based column and row indices.-} 
+
+    isMarked :: Int -> Int -> [[Int]] -> Bool
+    isMarked x y bd = not (isEmpty x y bd)
+   
+    {-Return the player of the stone placed on a place (x,y) of a board bd.
+    The x and y are 1-based column and row indices. -}
+    
+    isMarkedBy :: Int -> Int -> [[Int]] -> Int -> Bool
+    isMarkedBy x y bd p
+        | isEmpty x y bd = False
+        | otherwise = row y bd !! (x-1) == p
+   
+     -- Return the player of the stone placed on a place (x, y) of a board bd. (\Assuming not empty)                                                             
+    marker :: Int -> Int -> [[Int]] -> Int                                      
+    marker x y bd = row y bd !! (x - 1)       
+
+    {-Are all places of board bd marked, i.e., there is no empty place? -}
+    -- Check if board fully marked by stones or not.                            
+    isFull :: [[Int]] -> Bool                                                   
+    isFull bd =  length (filter (\x -> x == 0) (concat bd)) == 0     
+
+    ------------------------------------------------------------------------------------
+    -- Part 3: Determining the game outcome 
+
+    {-Is the game played on a board bd won by a player p? That is, does 
+    the board bd has a winning row for the player p? -}
+    isWonBy :: [[Int]] -> Int -> Bool
+    isWonBy bd p                                                                
+        | elem True ([hasWinSeq (row n bd) p | n <- [1 .. size bd]]) = True -- \Horizontal                                                                      
+        | elem True ([hasWinSeq (col n bd) p | n <- [1 .. size bd]]) = True -- \Vertical      
+
+        | elem True ([hasWinSeq (diagonal n 1 bd) p | n <- [1 .. size bd]]) = True -- Top Diagonal                                                             
+        | elem True ([hasWinSeq (diagonal 1 n bd) p | n <- [1 .. size bd]]) = True -- Bottom Diagonal     
+
+        | elem True ([hasWinSeq (diagonal n 1 (reverse bd)) p | n <- [1 .. size bd]]) = True -- Reverse Diagonal                                               
+        | elem True ([hasWinSeq (diagonal 1 n (reverse bd)) p | n <- [1 .. size bd]]) = True -- Reverse Diagonal                                               
+        | otherwise = False                     
+    
+    
+    -- Check if given row/col has a win sequence.                               
+    hasWinSeq :: [Int] -> Int -> Bool                                           
+    hasWinSeq [] _ = False                                                      
+    hasWinSeq (h : t) p                                                         
+        | h == p && length t >= 4 && length ([n | n <- take 4 t, n == p]) == 4 = True                                                                          
+        | otherwise = hasWinSeq t p                                             
+                                       
+
+    {-Is the game played on a board bd ended in a draw? -}
+    isDraw :: [[Int]] -> Bool
+    isDraw bd = isFull bd
+
+
+    {-Is the game played on a board bd over? -}
+    isGameOver :: [[Int]] -> Bool
+    isGameOver bd 
+        | isWonBy bd mkPlayer = True
+        | isWonBy bd mkOpponent = True
+        | isDraw bd = True
+        | otherwise = False
+
+    ------------------------------------------------------------------------------------
+    -- Part 4 : Converting to a string for printing 
+
+    {- Return a string representation of a board bd. This is a
+    higher-order function, and playerToChar is a function that
+    converts a player to a character representation, e.g., 'O' and
+    'X' (see Part II below). A formatted sample return value is
+    shown below; it is produced with the playerToChar function that
+    maps the first player to 'O', the second player to 'X', and
+    others to '.'. -}
+
+
+    --  Return a string representation of a board bd
+    boardToStr :: (Int -> Char) -> [[Int]] -> String
+    boardToStr playerToChar bd = h1 ++ h2 ++ rows
+        where 
+            h1 = "x " ++ concat([show (mod i 10 ) ++ " "| i <- [1 .. size bd]]) ++ "\n"
+            h2 = "y " ++ concat(["-" | i <- [1 .. ((size bd) * 2)]]) ++ "\n"
+            rows = concat([show(mod i 10) ++ "|" ++ rowToStr playerToChar (row i bd) | i <- [1 .. size bd]])
+
+
+    -- Return a string representation of a row.
+    rowToStr :: (Int -> Char) -> [Int] -> String
+    rowToStr _ [] = "\n"
+    rowToStr f (h : t) = [f h] ++ " " ++ rowToStr f t
+
+
+
+
+    {- Sample output provided 
+     " x 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5\n
+      y ------------------------------\n
+      1| . . . . . . . . . . . . . . .\n
+      2| . . . . . . . . . . . . . . .\n
+      3| . . . . . . . . . . . . . . .\n
+      4| . . . . . . . . . . . . . . .\n
+      5| . . . . . . . . . . . . . . .\n
+      6| . . . . . . . . O . . . . . .\n
+      7| . . . . . . . X X . . . . . .\n
+      8| . . . . . . X O O . . . . . .\n
+      9| . . . . . . . . . . . . . . .\n
+      0| . . . . . . . . . . . . . . .\n
+      1| . . . . . . . . . . . . . . .\n
+      2| . . . . . . . . . . . . . . .\n
+      3| . . . . . . . . . . . . . . .\n
+      4| . . . . . . . . . . . . . . .\n
+      5| . . . . . . . . . . . . . . .\n" 
+      
+      O's turn: enter x y (1-15 or -1 to quit)? 7 9 -}
+
+
+  
--- Main.hs
+++ Main.hs
@@ -1,68 +1,85 @@
 module Main where 
-    import Game
-    import System.IO
-    import System.Random 
-    
-    {-Main function to play an omok game between two human players.
-    It returns an IO() value. 
-    The dimension of the board is 15x15, and user inputs are read from the standard input 
-    (see the readXY function below) and outputs like the board state and the game
-    outcome are printed on the standard output.-}
-    main :: IO ()
-    main = do
-        let bd = mkBoard 15
-        putStrLn (boardToStr playerToChar bd)
-        mainHelper bd
+    import System.IO 
+    import Board
 
-   
-    mainHelper :: [[Int]] -> IO()
-    mainHelper bd = do 
-        playerCoordinates <- readXY bd 1
-        let bdPlayerMove = mark (fst playerCoordinates) (snd playerCoordinates) bd 1
-        putStrLn (boardToStr playerToChar bdPlayerMove)
-        if isWonBy bdPlayerMove 1 then do
-            putStrLn "Player 1 is the winner!"
-            return ()
-        else do
-            opponentCoordinates <- readXY bdPlayerMove 2
-            let bdOpponentMove = mark (fst opponentCoordinates) (snd opponentCoordinates) bdPlayerMove 2
-            putStrLn (boardToStr playerToChar bdOpponentMove)
-            if isWonBy bdOpponentMove 2 then do
-                putStrLn "Player 2 is the winner!"
-                return ()
-            else if isDraw bdOpponentMove then do
-                putStrLn "Game is a draw!"
-                return ()
-            else do
-                mainHelper  bdOpponentMove
+    ------------------------------------------------------------------------------------
+    -- Part 1: Reading user inputs and printing outputs 
     
-    {-Return a character representation of a player p. It returns a Char
-    value. This function may be used to print the current state of
-    a board (see the boardToStr function in Part I). -}
+    {-
+    Return a character representation of a player p. It returns a Char value. 
+    This function may be used to print the current state of a board. -}
     playerToChar :: Int -> Char
     playerToChar p
         | p == 1 = 'O'      -- Player
         | p == 2 = 'X'      -- Computer
         | otherwise = '.'   -- Empty space
 
-    {-Read a 1-based pair of indices (x, y) for player p, denoting an 
-    unmarked place in a board bd. The function reads inputs from the
-    standard input (stdin) and returns an IO value such as IO(Int,Int)
-    or IO(Integer,Integer). -}
+
+    {-
+    Read a 1-based pair of indices (x, y) for player p, denoting an unmarked place in a board bd. 
+    The function reads inputs from the standard input (stdin) 
+    returns an IO value such as IO(Int,Int) or IO(Integer,Integer). -}
     readXY :: [[Int]] -> Int -> IO(Int, Int)
     readXY bd p = do
-        putStrLn ("Player " ++ show p ++ ", enter x and y coordinates separated by spaces (must be >= 1 and <= " ++ show(size bd) ++ "): ")
+        print (size bd)
+        putStrLn ("Player " ++ show p ++ " turn: enter x y coordinates (1-15 or -1 to quit)?") 
         line <- getLine
-        let enteredValues = map read (words line)
-        if length enteredValues /= 2 || enteredValues !! 0 < 1 || enteredValues!!0 > (size bd) || enteredValues!!1 < 0 || enteredValues!!1 > (size bd) then do
-            putStrLn "Invalid input!"
-            readXY bd p
-        else if isMarked (enteredValues !! 0) (enteredValues !! 1) bd then do
+
+        let enteredCoordinates = map read (words line)
+        
+        if length enteredCoordinates /= 2 || head enteredCoordinates  < 1 || head enteredCoordinates > size bd || enteredCoordinates !! 1 < 0 || enteredCoordinates !! 1 > size bd then 
+            do
+                putStrLn "Invalid input!"
+                readXY bd p
+        
+        else if isMarked (head enteredCoordinates) (tail enteredCoordinates !! 1) bd then do
             putStrLn "Place is not empty!"
             readXY bd p
+        
         else
-            return (enteredValues !! 0, enteredValues !! 1)
+            return (head enteredCoordinates, enteredCoordinates !! 1)
     
 
+    ------------------------------------------------------------------------------------
+    -- Part 2 Playing the game 
+
+    {-
+    Main function to play an omok game between two human players.
+    It returns an IO() value. 
+    The dimension of the board is 15x15
+    User inputs are read from the standard input
+    Outputs the board, state the game outcome are printed on standard output. -}
+
+    main :: IO ()
+    main =  do
+            let bd = mkBoard 15
+            mainHelper bd
 
     
+    mainHelper :: [[Int]] -> IO()
+    mainHelper bd = do 
+                    playerCoordinates <- readXY bd 1
+                    let bdPlayerMove = uncurry mark playerCoordinates bd 1
+                    putStrLn (boardToStr playerToChar bdPlayerMove)
+
+                    if isWonBy bdPlayerMove 1 then do
+                        putStrLn "Player 1 is the winner!"
+                        return ()
+        
+                    else do
+                        opponentCoordinates <- readXY bdPlayerMove 2
+                        let bdOpponentMove = uncurry mark opponentCoordinates bdPlayerMove 2
+                        putStrLn (boardToStr playerToChar bdOpponentMove)
+                        
+                        if isWonBy bdOpponentMove 2 then do
+                            putStrLn "Player 2 is the winner!"
+                            return ()
+                        
+                        else if isDraw bdOpponentMove then do
+                            putStrLn "Game is a draw!"
+                            return ()
+                        
+                        else do
+                            mainHelper  bdOpponentMove
+
+            
